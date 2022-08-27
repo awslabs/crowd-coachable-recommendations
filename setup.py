866b3858d4649ba899ede32ab1447faf3cd2072f
Yifei Ma <yifeim@amazon.com>
2022-08-26 22:50:30 -0700

    graph conv should be fixed from upstream

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     install_requires=[
         "datasets >= 2.4.0",
         "shap >= 0.41.0",
-        'psutil',
         'recurrent-intensity-model-experiments @ git+https://github.com/awslabs/recurrent-intensity-model-experiments@main',
     ],
 )
