ae7537b0fd1e8c8a505f16d69bb05663db13533d
Yifei Ma <yifeim@amazon.com>
2022-09-13 17:06:26 -0700

    add option for independent explanations

--- src/ccrec/util/shap_explainer.py
+++ src/ccrec/util/shap_explainer.py
@@ -74,19 +74,20 @@ class I2IExplainer:
     tokenizer: typing.Any
     fixed_context: int = 0  # 0 yields sparser results
     max_length: int = 128
+    independent_explainations: bool = False
 
     @property
     def tokenizer_kw(self):
         return dict(padding=True, return_tensors='pt', max_length=self.max_length, truncation=True)
 
-    def _get_unitary_loss(self, texts):
+    def _get_unitary_utility(self, texts):
         if hasattr(self.item_tower, 'IS_AUTO_ENCODER') and self.item_tower.IS_AUTO_ENCODER:
             _inputs = self.tokenizer(texts.tolist(), **self.tokenizer_kw)
             loss = self.item_tower(**_inputs, output_step='dict')[0].cpu().numpy()
             return -loss.ravel()
         return np.ravel(1)
 
-    def _get_pairwise_loss(self, x, cand_texts):
+    def _get_pairwise_utility(self, x, cand_texts):
         _inputs = self.tokenizer(cand_texts.tolist(), **self.tokenizer_kw)
         y = self.item_tower(**_inputs)
         return (x * y).sum(-1).cpu().numpy()
@@ -94,11 +95,14 @@ class I2IExplainer:
     @torch.no_grad()
     def __call__(self, given, cand_texts=None):
         if cand_texts is None:
-            explainer = shap.Explainer(self._get_unitary_loss, self.tokenizer)
+            explainer = shap.Explainer(self._get_unitary_utility, self.tokenizer)
             return explainer(given, fixed_context=self.fixed_context)
-        else:
+        elif self.independent_explainations:
+            explainer = shap.Explainer(self._get_unitary_utility, self.tokenizer)
+            return explainer(cand_texts, fixed_context=self.fixed_context)
+        else:  # conditional explanations
             _inputs = self.tokenizer(given, **self.tokenizer_kw)
             x = self.item_tower(**_inputs)
-            f = functools.partial(self._get_pairwise_loss, x)
+            f = functools.partial(self._get_pairwise_utility, x)
             explainer = shap.Explainer(f, self.tokenizer)
             return explainer(cand_texts, fixed_context=self.fixed_context)
