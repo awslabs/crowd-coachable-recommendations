0bc96f548d908e521e711147d60780d8636bb505
Yifei Ma <yifeim@amazon.com>
2022-11-14 15:37:36 -0800

    check in i2iimage_env and reformat shap_explainer

--- src/ccrec/util/shap_explainer.py
+++ src/ccrec/util/shap_explainer.py
@@ -2,12 +2,15 @@ import matplotlib.pyplot as plt, numpy as np
 from matplotlib import transforms
 import dataclasses, typing, torch
 import shap, functools
-from shap.plots._text import unpack_shap_explanation_contents, process_shap_values, colors
+from shap.plots._text import (
+    unpack_shap_explanation_contents,
+    process_shap_values,
+    colors,
+)
 
 
 def values_min_max(values, base_values):
-    """ Used to pick our axis limits.
-    """
+    """Used to pick our axis limits."""
     fx = base_values + values.sum()
     xmin = fx - values[values > 0].sum()
     xmax = fx - values[values < 0].sum()
@@ -19,20 +22,23 @@ def values_min_max(values, base_values):
     return xmin, xmax, cmax
 
 
-def get_tokens_and_colors(shap_values, num_starting_labels=0, grouping_threshold=0.01, separator=''):
+def get_tokens_and_colors(
+    shap_values, num_starting_labels=0, grouping_threshold=0.01, separator=""
+):
     # set any unset bounds
     xmin, xmax, cmax = values_min_max(shap_values.values, shap_values.base_values)
 
     values, clustering = unpack_shap_explanation_contents(shap_values)
     tokens, values, group_sizes = process_shap_values(
-        shap_values.data, values, grouping_threshold, separator, clustering)
+        shap_values.data, values, grouping_threshold, separator, clustering
+    )
 
     return tokens, 0.5 + 0.5 * values / (cmax + 1e-8)
 
 
 def rainbow_text(x, y, ls, lc, width=40, nrows=4, **kw):
-    """ https://stackoverflow.com/a/9185851
-    https://stackoverflow.com/q/23696898 """
+    """https://stackoverflow.com/a/9185851
+    https://stackoverflow.com/q/23696898"""
 
     t = plt.gca().transData
     fig = plt.gcf()
@@ -42,20 +48,25 @@ def rainbow_text(x, y, ls, lc, width=40, nrows=4, **kw):
     cur_rows = 0
 
     for i, (s, c) in enumerate(zip(ls, lc)):
-        text = plt.text(x, y, s, color=c if isinstance(c, str) else 'black',
-                        transform=t, **kw)
+        text = plt.text(
+            x, y, s, color=c if isinstance(c, str) else "black", transform=t, **kw
+        )
         if not isinstance(c, str):
             color = colors.red_transparent_blue(c)
-            text.set_bbox(dict(facecolor=color, edgecolor='none', pad=0, boxstyle='round'))
+            text.set_bbox(
+                dict(facecolor=color, edgecolor="none", pad=0, boxstyle="round")
+            )
         text.draw(fig.canvas.get_renderer())
         ex = text.get_window_extent()
         if cur_words + len(s) >= width:
-            t = transforms.offset_copy(text._transform, x=-cur_x, y=-ex.height * 1.2, units='dots')
+            t = transforms.offset_copy(
+                text._transform, x=-cur_x, y=-ex.height * 1.2, units="dots"
+            )
             cur_words = 0
             cur_x = 0
             cur_rows += 1
         else:
-            t = transforms.offset_copy(text._transform, x=ex.width, units='dots')
+            t = transforms.offset_copy(text._transform, x=ex.width, units="dots")
             cur_words += len(s)
             cur_x += ex.width
         if cur_rows >= nrows:
@@ -63,7 +74,7 @@ def rainbow_text(x, y, ls, lc, width=40, nrows=4, **kw):
 
 
 def plot_shap_values(x, y, shap_values, **kw):
-    assert not hasattr(shap_values, '__iter__'), "single instance at a time"
+    assert not hasattr(shap_values, "__iter__"), "single instance at a time"
     ls, lc = get_tokens_and_colors(shap_values)
     rainbow_text(x, y, ls, lc, **kw)
 
@@ -78,13 +89,19 @@ class I2IExplainer:
 
     @property
     def tokenizer_kw(self):
-        return dict(padding=True, return_tensors='pt', max_length=self.max_length, truncation=True)
+        return dict(
+            padding=True,
+            return_tensors="pt",
+            max_length=self.max_length,
+            truncation=True,
+        )
 
     def _get_unitary_utility(self, texts):
         from ccrec.models.item_tower import VAEItemTower
+
         if isinstance(self.item_tower, VAEItemTower):
             _inputs = self.tokenizer(texts.tolist(), **self.tokenizer_kw)
-            loss = self.item_tower(**_inputs, output_step='dict')[0].cpu().numpy()
+            loss = self.item_tower(**_inputs, output_step="dict")[0].cpu().numpy()
             return -loss.ravel()
         return np.ones(len(texts))
 
